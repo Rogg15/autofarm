bcc629e8bd6deb43733febe6eb5244c8cf0c7ba1
Rogg15 <96954305+Rogg15@users.noreply.github.com>
2022-02-04 00:32:22 -0800

    Add files via upload

---
+++ Display_Stats.lua
@@ -0,0 +1,21 @@
+
+
+--[[
+
+Display Stats  -  Made by Bradley#6804
+    > FPS
+    > GPU
+    > CPU
+    > PING
+
+Open Soure. . . Feel free to check my shitty script(s). . .
+
+]]--
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AreexYT/AG-Hub---v2/main/Settings/FPS.lua'))()
+    wait()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AreexYT/AG-Hub---v2/main/Settings/GPU.lua'))()
+    wait()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AreexYT/AG-Hub---v2/main/Settings/CPU.lua'))()
+    wait()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AreexYT/AG-Hub---v2/main/Settings/Ping.lua'))()
+    wait()
